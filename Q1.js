6804cfe206ff09d895ed3ce10cfb48513f221bb4
gokulramcoddle <gokulram.t@coddletech.com>
2025-02-20 16:40:22 +0530

    A1

--- Q1.js
+++ Q1.js
@@ -1,16 +1,16 @@
 // Q1)Create variables with different data types available in JS and print their data types along with each variable name in the console. 
 
-var num= 2002;
+let num= 2002;
 console.log("variable: num " +" | " +"Datatype : " + typeof(num));
-var str="Ram";
+let str="Ram";
 console.log("variable: str " +" | " +"Datatype : " + typeof(str));
-var bool=true;
+let bool=true;
 console.log("variable: bol " +" | " +"Datatype : " + typeof(bool));
-var obj={name:"Gokul",lastname:"Ram"};
+let obj={name:"Gokul",lastname:"Ram"};
 console.log("variable: obj " +" | " +"Datatype : " + typeof(obj));
-var fun=function(){console.log("function example")};
+let fun=function(){console.log("function example")};
 console.log("variable: fun " +" | " +"Datatype : " + typeof(fun));
-var arr=[1,2,3,4];
+let arr=[1,2,3,4];
 console.log("variable: arr " +" | " +"Datatype : " + typeof(arr));
-var n=null;
+let n=null;
 console.log("variable: n   " +" | " +"Datatype : " + typeof(n));
--- Q3.js
+++ Q3.js
@@ -1,92 +1,71 @@
-// Q3)["1", "2", "3", "4", "5", "6", "7"]
+// Q3 a) Remove the number "6" from the array and console the length of the array.
+let numbers = ["1", "2", "3", "4", "5", "6", "7"];
+let index = numbers.indexOf("6");
 
-//Q3 a)Remove the number "6" from the array and console the length of the array.
-
-var arr=["1", "2", "3", "4", "5", "6", "7"];
-var idx=arr.indexOf("6");
-arr.splice(idx,1);
-console.log(arr.length);
-
-//Q3 b) Convert all the items of the array to data type number and console each item's data type, using any of the array iteration methods provided by JS for iteration.
-var arr=["1", "2", "3", "4", "5", "6", "7"];
-for(var a in arr){
-    var v=Number(arr[a]);
-    console.log(v +" "+ typeof v);
+if (index !== -1) {
+    numbers.splice(index, 1);
 }
+console.log("Array length after removal:", numbers.length);
 
-//Q3 c) Remove the last three items of the array, use JS provided array method, console the array and then add "one" and "two" (strings) to the beginning of the array and console the array. 
-var arr=["1", "2", "3", "4", "5", "6", "7"];
-arr.splice(-3);
-console.log("Removed last 3 values");
-for(var a in arr){
-    console.log(arr[a]);
-}
-var arr=["1", "2", "3", "4", "5", "6", "7"];
-console.log("Added 2 string in front");
-arr.unshift("one","two");
-for(var a in arr){
-    console.log(arr[a]);
-}
-
-// Q3 d)Using any one of the array iteration methods console the string concatenation of all items of the array and also console the sum of all the items ( convert to integer before calculating)
-var arr=["1", "2", "3", "4", "5", "6", "7"];
-var comb="";
-console.log("concatination of string");
-for( var c in arr){
-    comb+=arr[c];
-}
-console.log(comb);
-console.log("Addition of all value");
-var sum=0;
-for( var n in arr){
-    sum+=Number(arr[n]);
-}
-console.log(sum);
-
-//Q3 e)Filter out item "3" from the array and console the array (use array method)
-var arr=["1", "2", "3", "4", "5", "6", "7"];
-var filArr=arr.filter(function(val){
-    if(val!="3"){
-        return true;
-    }
-    else false;
+// Q3 b) Convert all items to numbers and console each item's data type.
+numbers = ["1", "2", "3", "4", "5", "6", "7"];
+numbers.forEach(num => {
+    let convertedNum = Number(num);
+    console.log(`${convertedNum} - ${typeof convertedNum}`);
 });
-for(var i in filArr){
-console.log(filArr[i]);
-}
 
-//Q3 f)Iterate the array and console the item, when the item is either "3", "6" or "7"
-var arr=["1", "2", "3", "4", "5", "6", "7"];
-for(var i in arr){
-    if(arr[i]=="3" || arr[i]=="6" || arr[i]=="7"){
-     console.log(arr[i]);
+// Q3 c) Remove the last three items and console the array, then add "one" and "two" to the beginning.
+numbers = ["1", "2", "3", "4", "5", "6", "7"];
+numbers.splice(-3);
+console.log("Array after removing last 3 items:", numbers);
+
+numbers.unshift("one", "two");
+console.log("Array after adding 'one' and 'two':", numbers);
+
+// Q3 d) Concatenate all items as a string and sum all numbers after conversion.
+numbers = ["1", "2", "3", "4", "5", "6", "7"];
+
+let concatenatedString = numbers.join("");
+console.log("Concatenated String:", concatenatedString);
+
+let totalSum = numbers.reduce((sum, num) => sum + Number(num), 0);
+console.log("Sum of all values:", totalSum);
+
+// Q3 e) Filter out "3" from the array and console the updated array.
+numbers = ["1", "2", "3", "4", "5", "6", "7"];
+let filteredNumbers = numbers.filter(num => num !== "3");
+console.log("Array after filtering out '3':", filteredNumbers);
+
+// Q3 f) Console the item if it is "3", "6", or "7".
+numbers.forEach(num => {
+    if (["3", "6", "7"].includes(num)) {
+        console.log(num);
+    }
+});
+
+// Q3 g) Compare two arrays and console values with the same data type.
+let array1 = ["1", "2", "3", "4", "5", "6", "7"];
+let array2 = [1, 2, "3", 4, 5, 6, "7"];
+
+for (let i = 0; i < array1.length; i++) {
+    if (typeof array1[i] === typeof array2[i]) {
+        console.log("Matching Type:", array1[i]);
     }
 }
 
-// Q3 g)[1, 2, "3", 4, 5, 6, "7"]  Compare this array with the above given array and console only if both items of the array have the same data type. (Compare each item of this array with each item of the other array)
-var arr=["1", "2", "3", "4", "5", "6", "7"];
-var brr=[1, 2, "3", 4, 5, 6, "7"]
-for(var i=0;i<arr.length;i++){
-   for(var j=0;j<brr.length;j++){
-       if(arr[i] === brr[j] && typeof arr[i] == typeof brr[i]){
-           console.log(arr[i]);
-       }
-   }
-}
+// Q3 h) Multiply each item by its index and console results greater than 40.
+let numArray = [0, 2, 3, 7, 5, 6, 8];
 
-//Q3 h) [0,2,3,7,5,6,8] iterate the array, multiply each item by its index value, and console the result only if the result is greater than 40.
-var arr=[0,2,3,7,5,6,8];
-for(var i in arr){
-    var mul=i*arr[i];
-    if(mul>40){
-        console.log("Digit: "+arr[i]+"  |  "+"value: "+mul);
+numArray.forEach((num, index) => {
+    let result = num * index;
+    if (result > 40) {
+        console.log(`Digit: ${num} | Value: ${result}`);
     }
-}
+});
 
-//Q3 i) Create two arrays with five items each, merge them into a single array, and then console them.
-var a1=[0,1,2,3,4];
-var a2=[5,6,7,8,9];
-var mergedArr=a1.concat(a2);
-for(var i in mergedArr){
-console.log(mergedArr[i]);
-}
+// Q3 i) Merge two arrays and console the result.
+let firstArray = [0, 1, 2, 3, 4];
+let secondArray = [5, 6, 7, 8, 9];
+
+let mergedArray = [...firstArray, ...secondArray];
+console.log("Merged Array:", mergedArray);
